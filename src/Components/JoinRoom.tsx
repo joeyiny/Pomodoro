b8b16083c6b3b71062b4f0591cddfb6b253492d2
Joey Iny <joeyainy@gmail.com>
2022-04-29 15:16:40 -0700

    Add Start New Room Button

--- src/Components/JoinRoom.tsx
+++ src/Components/JoinRoom.tsx
@@ -15,24 +15,29 @@ const JoinRoom = ({ socket }: { socket: Socket }) => {
           value={userNameInput}
           onChange={(e) => setUserNameInput(e.target.value)}
         />
-        <div className="flex flex-row gap-2">
-          <input
-            className="text-gray-900 flex-grow px-1 py-0.5"
-            placeholder="Enter room code"
-            type="text"
-            value={roomCodeInput}
-            onChange={(e) => setRoomCodeInput(e.target.value)}
-          />
-          <button
-            onClick={() => {
-              socket.emit("join-room", {
-                roomCode: roomCodeInput,
-                userName: userNameInput,
-              });
-            }}
-            className="border-2 border-white rounded  w-min px-2 py-1">
-            Join
+        <div className="flex flex-row gap-x-2">
+          <button className="bg-white text-gray-800 px-2">
+            Start New Room
           </button>
+          <div className="flex-grow flex flex-row gap-1">
+            <input
+              className="text-gray-900 px-1 py-0.5"
+              placeholder="Enter room code"
+              type="text"
+              value={roomCodeInput}
+              onChange={(e) => setRoomCodeInput(e.target.value)}
+            />
+            <button
+              onClick={() => {
+                socket.emit("join-room", {
+                  roomCode: roomCodeInput,
+                  userName: userNameInput,
+                });
+              }}
+              className="border-2 border-white rounded  w-min px-2 py-1">
+              Join
+            </button>
+          </div>
         </div>
       </div>
     </div>
