3e956e1d6101a47f9fe94fd945ea6bf1ef817f0d
Joey Iny <joeyainy@gmail.com>
2022-04-28 19:31:39 -0700

    Fix bug where selected task isn't persistent

--- src/App.tsx
+++ src/App.tsx
@@ -74,7 +74,7 @@ function App() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
     localStorage.setItem(
       "selectedTaskIndex",
-      selectedTaskIndex ? selectedTaskIndex.toString() : "null"
+      selectedTaskIndex !== null ? selectedTaskIndex?.toString() : "null"
     );
     localStorage.setItem(
       "completedPomodoros",
