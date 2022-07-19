14986747b9d45e26f6bd58c1bb7513d6690604e4
joeyiny <joeyainy@gmail.com>
2022-07-18 17:16:17 -0700

    Center Timer on screen

--- src/screens/SoloPomo.tsx
+++ src/screens/SoloPomo.tsx
@@ -8,8 +8,8 @@ const SoloPomo = () => {
   const { socket } = useContext(RoomContext);
 
   return (
-    <div className="flex gap-2 max-w-7xl m-auto px-3">
-      <div className="flex flex-shrink-0 gap-2 flex-col w-[18rem]">
+    <div className="max-w-7xl m-auto">
+      <div className="flex flex-shrink-0 gap-2 flex-col w-[22rem] m-auto">
         {/* {newUserEffectOn && <NewUserNotification />} */}
         {/* <Chat /> */}
         <Timer socket={socket} />
