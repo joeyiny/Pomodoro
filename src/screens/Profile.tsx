9f6634be01611171196b3b269bc9f07b2d50c2c2
joeyiny <joeyainy@gmail.com>
2022-07-26 10:36:29 -0700

    Set up populated data for profile pomo calendar

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -5,6 +5,9 @@ import { AuthContext } from "../context/AuthContext";
 const Profile = () => {
   const { email } = useParams();
   const [user, setUser] = useState<any>({});
+  const [completedPomodoros, setCompletedPomodoros] = useState<
+    Array<{ date: string | number | Date }>
+  >([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
@@ -13,82 +16,83 @@ const Profile = () => {
       let user = await response.json();
       setUser(user);
       setIsLoading(false);
+      if (user.completedPomodoros?.length > 0) {
+        let today = new Date();
+        today.setHours(0, 0, 0, 0);
+        let oneWeekAgo = new Date(
+          today.getFullYear(),
+          today.getMonth(),
+          today.getDate() - 6
+        );
+        let newPomos = user.completedPomodoros.filter(
+          (pomo: { date: string | number | Date }) => {
+            let pomoDate = new Date(pomo.date);
+            return pomoDate >= oneWeekAgo;
+          }
+        );
+        setCompletedPomodoros(newPomos);
+      }
     };
     fetchUser();
   }, [email]);
 
-  useEffect(() => {
-    if (user.completedPomodoros?.length > 0) {
-      let newPomos = user.completedPomodoros.filter(
-        (pomo: { date: string | number | Date }) => {
-          let pomoDate = new Date(pomo.date);
-          let randomDate = new Date("2022-06-08");
-          return pomoDate <= randomDate;
-        }
+  let displayPomodoroCalendar = () => {
+    let today = new Date();
+    let output = [];
+    for (let i = 6; i >= 0; i--) {
+      let date = new Date(
+        today.getFullYear(),
+        today.getMonth(),
+        today.getDate() - i
       );
-      console.log(newPomos);
-      console.log(new Date());
-    }
-  }, [user]);
+      let currentPomoList = completedPomodoros.filter((pomo) => {
+        let pomoDate = new Date(pomo.date);
+        pomoDate.setHours(0, 0, 0, 0);
+        return pomoDate.getTime() === date.getTime();
+      });
+      let numberOfPomos = currentPomoList.length;
+      output.push(
+        <div className="flex-1 h-full">
+          {i === 0 ? (
+            <span className="font-semibold text-gray-50">Today</span>
+          ) : (
+            <span className="text-gray-400">
+              {date.toLocaleDateString("en-us", {
+                month: "short",
+                day: "numeric",
+              })}
+            </span>
+          )}
 
-  let displayPomodoros = () => {
-    return <p>hi</p>;
+          <div className="flex justify-center flex-col align-middle border border-gray-500 rounded-lg w-full h-full">
+            {numberOfPomos > 0 ? (
+              <span className="text-primary text-3xl font-semibold">
+                {numberOfPomos}
+              </span>
+            ) : (
+              <span className="text-gray-500 text-3xl font-semibold">0</span>
+            )}
+          </div>
+        </div>
+      );
+    }
+    return output;
   };
 
   if (isLoading) return <div>loading</div>;
   return (
     <div className="flex flex-col gap-6">
       <h3 className="text-xl font-semibold text-gray-50">{user.displayName}</h3>
-      {/* <p>All time completed pomodoros: {user.completedPomodoros.length}</p> */}
-      {/* {displayPomodoros()} */}
       <div className="flex flex-col gap-2">
         <p>
           Past 7 days:{" "}
-          <span className="text-primary font-semibold">18 pomos</span>.
+          <span className="text-primary font-semibold">
+            {completedPomodoros.length} pomos
+          </span>
+          .
         </p>
-        <div className="flex flex-row gap-2 justify-between  w-[464px] h-14 m-auto">
-          <div className="flex-1 h-full">
-            <span className="text-gray-400">Jul 20</span>
-            <div className="flex justify-center flex-col align-middle border border-gray-500 rounded-lg w-full h-full">
-              <span className="text-gray-500 text-3xl font-semibold">0</span>
-            </div>
-          </div>
-          <div className="flex-1 h-full">
-            <span className="text-gray-400">Jul 21</span>
-            <div className="flex justify-center flex-col align-middle border border-gray-500 rounded-lg w-full h-full">
-              <span className="text-primary text-3xl font-semibold">3</span>
-            </div>
-          </div>
-          <div className="flex-1 h-full">
-            <span className="text-gray-400">Jul 22</span>
-            <div className="flex justify-center flex-col align-middle border border-gray-500 rounded-lg w-full h-full">
-              <span className="text-primary text-3xl font-semibold">6</span>
-            </div>
-          </div>
-          <div className="flex-1 h-full">
-            <span className="text-gray-400">Jul 23</span>
-            <div className="flex justify-center flex-col align-middle border border-gray-500 rounded-lg w-full h-full">
-              <span className="text-primary text-3xl font-semibold">3</span>
-            </div>
-          </div>
-          <div className="flex-1 h-full">
-            <span className="text-gray-400">Jul 24</span>
-            <div className="flex justify-center flex-col align-middle border border-gray-500 rounded-lg w-full h-full">
-              <span className="text-gray-500 text-3xl font-semibold">0</span>
-            </div>
-          </div>
-          <div className="flex-1 h-full">
-            <span className="text-gray-400">Jul 25</span>
-            <div className="flex justify-center flex-col align-middle border border-gray-500 rounded-lg w-full h-full">
-              <span className="text-primary text-3xl font-semibold">4</span>
-            </div>
-          </div>
-          <div className="flex-1 h-full">
-            <span className="font-semibold text-gray-50">Today</span>
-            <div className="flex justify-center flex-col align-middle border border-gray-50 rounded-lg w-full h-full">
-              <span className="text-primary text-3xl font-semibold">2</span>
-            </div>
-          </div>
+        <div className="flex flex-row gap-2 justify-between w-[464px] h-14 m-auto">
+          {displayPomodoroCalendar()}
         </div>
       </div>
     </div>
