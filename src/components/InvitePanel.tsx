86fe60154a24fc64d5084909ada4ba76d33cf72e
joey iny <joeyainy@gmail.com>
2023-07-02 19:37:18 -0700

    Change the share from URL -> room code temporarily
    because we lost https, so it gets messed up. will fix later

--- src/components/InvitePanel.tsx
+++ src/components/InvitePanel.tsx
@@ -1,25 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router";
 import { AiOutlineCopy } from "react-icons/ai";
 
 const InvitePanel = () => {
   const { roomCode } = useParams();
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = async (text: string) => {
+    await navigator.clipboard.writeText(text);
+    setCopied(true);
+
+    // reset copied indicator after 2 seconds
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  };
 
   return (
-    <div className="mt-3 mb-2">
+    <div className="mt-3 mb-2 relative">
+      {" "}
+      {/* make this element relative */}
       <span className="text-gray-400 text-sm font-semibold">
-        Share this room link to invite a friend:
+        Share this room code with a friend:
       </span>
       <div
         className="flex flex-row gap-2 justify-between bg-gray-800 rounded-lg px-4 py-3 items-center text-gray-400 hover:text-gray-50 hover:cursor-pointer select-none"
-        onClick={() => {
-          navigator.clipboard.writeText(`https://pomo.wtf/${roomCode}`);
-        }}>
+        onClick={() => handleCopy(`${roomCode}`)}>
         <span className="text-sm inline-block text-left font-semibold">
-          https://pomo.wtf/{roomCode}
+          {roomCode}
         </span>
         <AiOutlineCopy className="inline-block" size="2em" />
       </div>
+      {copied && (
+        <span
+          style={{
+            position: "absolute",
+            top: "20px",
+            right: "0",
+            opacity: copied ? "1" : "0",
+          }}
+          className="bg-green px-2 py-1  text-green-dark rounded transition-opacity">
+          Copied to clipboard!
+        </span>
+      )}
     </div>
   );
 };
